84bf02b27ba64f4ef80f0f1bf37418f6201a0603
rockfe99 <rockfe99@gmail.com>
2025-11-18 17:28:12 +0900

    no message

--- movie_app_full/src/main/java/com/example/movieapp/repository/MovieInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.movieapp.repository;
-
-import com.example.movieapp.entity.MovieInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieInfoRepository extends JpaRepository<MovieInfo, Integer> {
-}
--- movie_app_full/src/main/java/com/example/movieapp/repository/MovieMemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.movieapp.repository;
-
-import com.example.movieapp.entity.MovieMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieMemberRepository extends JpaRepository<MovieMember, String> {
-}
